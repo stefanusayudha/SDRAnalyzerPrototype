4eb27768890e5164819d7c3a72f0294258d5eaa5
stefanus ayudha <stefanus.ayudha@gmail.com>
2023-01-02 13:06:25 +0700

    add pinch and band gap option

--- lib/compound_signal.m
+++ lib/compound_signal.m
@@ -4,6 +4,8 @@ classdef compound_signal
     signal_stream;
     sampling_rate;
     duration_in_second;
+    pinch;
+    band_gap;
   endproperties
 
   methods(Access=private)
@@ -46,8 +48,53 @@ classdef compound_signal
       P1(2:end-1) = 2*P1(2:end-1);
       f = obj.sampling_rate*(0:(L/2))/L;
 
+
+      # trim to pinch freq
+      lowest_pinch = obj.pinch - obj.band_gap;
+      highest_pinch = obj.pinch + obj.band_gap;
+
+      lowest_pinch_position = 1;
+      if(lowest_pinch > 0 && obj.pinch != -1 && obj.band_gap != -1)
+          i = 1;
+          found = false;
+          while(!found && i < length(f)-1)
+              
+              if ( f(i) > lowest_pinch )
+                  lowest_pinch_position = i-1;
+                  found = true;
+              else
+                  i = i+1;
+              endif
+          endwhile
+
+          %disp("lower pinch at i = ");
+          %disp(lowest_pinch_position);
+          %disp("value = ");
+          %disp(f(lowest_pinch_position));
+      endif
+      
+      highest_pinch_position = length(f)-1;
+      if(highest_pinch <= max(f) && obj.pinch != -1 && obj.band_gap != -1)
+          i = highest_pinch_position;
+          found = false;
+          while(!found && i>0)
+              
+              if ( f(i) < highest_pinch )
+                  highest_pinch_position = i+1;
+                  found = true;
+              else
+                  i = i-1;
+              endif
+          endwhile
+
+          %disp("highest pinch at i = ");
+          %disp(highest_pinch_position);
+          %disp("value = ");
+          %disp(f(highest_pinch_position));
+      endif
+
       subplot(2,plot_col,plot_col+2);
-      plot(f,P1);
+      plot(f(lowest_pinch_position:highest_pinch_position),P1(lowest_pinch_position:highest_pinch_position));
       xlabel ("freq");
       ylabel ("magnitude");
       title ("FFT");
@@ -57,7 +104,7 @@ classdef compound_signal
 
   methods
 
-    function obj = compound_signal(array_freq,sampling_rate,duration_in_second)
+    function obj = compound_signal(array_freq,sampling_rate,duration_in_second,pinch,band_gap)
       buffer = sin_signal(0,0,sampling_rate,duration_in_second).signal_stream;
 
       for i = array_freq
@@ -68,6 +115,8 @@ classdef compound_signal
       obj.signal_stream = buffer;
       obj.sampling_rate = sampling_rate;
       obj.duration_in_second = duration_in_second;
+      obj.pinch =  pinch;
+      obj.band_gap = band_gap;
 
     endfunction
 
